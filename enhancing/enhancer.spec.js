8f3fb9030c46f22505544f693c5ff79108a6e48d
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-13 18:25:09 -0500

    tests built out and tested for succeed, fail, and repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,5 +1,5 @@
 const enhancer = require('./enhancer.js');
-const { succeed } = require('./enhancer.js')
+const { repair, fail, succeed } = require('./enhancer.js')
 
 // test away!
  
@@ -7,10 +7,12 @@ const { succeed } = require('./enhancer.js')
 
 describe('enhancer module', () => {
     
-});
+
 
     // Succeed test suite/group
     describe('succeed function', () => {
+
+        // test
         it('The item\'s enhancement increases by 1', () => {
             
             // Arrange
@@ -52,3 +54,93 @@ describe('enhancer module', () => {
             expect(succeed(item)).toEqual(expected)
         });
     });
+
+    // Fail test suite/group
+    describe('fail function', () => {
+        
+        // test
+        it('If the item\'s enhancement is less than 15, the durability of the item is decreased by 5', () => {
+            
+            // Arrange
+            const expected = {
+                name: "big sword",
+                durability: 95,
+                enhancement: 10
+            };
+
+            const item = {
+                name: "big sword",
+                durability: 100,
+                enhancement: 10
+            };
+
+            // Assert
+            expect(fail(item)).toEqual(expected);
+        });
+
+         // Test
+         it('If the item\'s enhancement is 15 or more, the durability of the item is decreased by 10', () => {
+             
+            // Arrange
+            const expected = {
+                name: "big sword",
+                durability: 90,
+                enhancement: 15
+            };
+
+            const item = {
+                name: "big sword",
+                durability: 100,
+                enhancement: 15
+            };
+
+            // Assert
+            expect(fail(item)).toEqual(expected);
+         });
+
+         // Test
+         it('If the item\'s enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).', () => {
+             
+            // Arrange
+            const expected = {
+                name: "big sword",
+                durability: 90,
+                enhancement: 17
+            };
+
+            const item = {
+                name: "big sword",
+                durability: 100,
+                enhancement: 18
+            };
+
+            // Assert 
+            expect(fail(item)).toEqual(expected);
+         });
+    });
+
+    // Repair test suite/group
+    describe('repair function', () => {
+        
+        // Test
+        it('item durability should return to 100 after it\'s repaired', () => {
+            
+            // Arrange
+            const expected = {
+                name: "big sword",
+                durability: 100,
+                enhancement: 20
+            };
+
+            const item = {
+                name: "big sword",
+                durability: 5,
+                enhancement: 20
+            };
+
+            // Assert 
+            expect(repair(item)).toEqual(expected);
+        
+        });
+    });
+});
